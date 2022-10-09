5467578f74f068ae8fdd69254eb5f37a22fc8d23
Mahad Ahmed <44931361+MahadAhmed25@users.noreply.github.com>
2022-10-09 14:29:58 -0400

    Add files via upload

---
+++ src/assets/js/music-list.js
@@ -0,0 +1,44 @@
+let allMusic = [
+    {
+        name: "Marvin Gaye",
+        arist: "Charlie Puth ft. Meghan Trainor",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "This is Us",
+        arist: "Jimmie Allen & Noah Cyrus",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Heat Waves",
+        arist: "Glass Animals",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Ghost",
+        arist: "Justin Bieber",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Everytime we Touch",
+        arist: "Cascada",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Heartless",
+        arist: "Kanye West",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Be Alright",
+        arist: "Dean Lewis",
+        img: "music-7",
+        src: "music-7"
+    },
+];
